df48367fa9bb0f8cbb33ca5e0686d4171197037d
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-02-11 22:19:51 +0530

    Reverse a Five-Digit Number.

---
+++ fifth.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main(){
+    
+    int n,rn=0,m=0,t;
+    
+    scanf("%d",&n);
+    
+   
+    if(n<0){
+        m = n*-1;
+       while(m>0){
+           t = m%10;
+           rn = (rn*10)+t;
+           m = m/10;
+       }
+       printf("Reversed number: -%d",rn);
+    }
+     else if(n>99999 || n<10000){
+         printf("Invalid input! Please enter a five-digit number.");
+    }
+    else {
+       m = n;
+       while(m>0){
+           t = m%10;
+           rn = (rn*10)+t;
+           m = m/10;
+       }
+       printf("Reversed number: %d",rn);
+    }
+    
+    return 0;
+}
