174e19b7cfc92f26c7593e22611054eb372cb98e
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-06-07 21:22:27 +0530

    Write a c mplete C program to print the following pattern for n number of rows, where n is supplied externally:
    text
    *             *
    * *         * *
    * * *     * * *
    * * * * * * * *
    * * * * * * * *

--- imp/pattern1.c
+++ imp/pattern1.c
@@ -1,26 +1,24 @@
 #include<stdio.h>
-int main(){
+int main() {
     int n;
     printf("Enter the number of rows: ");
-    scanf("%d",&n);
-    
-    if(n<1 || n>20)
-        printf("Invalid input! Enter a number between 1 and 20.\n");
-    else {
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=1;j++){
-                printf("*");
-            }
-            for(int j=1;j<=(2*(n-i));j++){
-                printf(" ");
-            }
-            for(int j=1;j<=1;j++){
-                printf("* ");
-            }
-            printf("\n");
-        }
+    scanf("%d", &n);
+    if(n<1) {
+        printf("Number of rows must be at least 1.\n");
+        return 1;
     }
-    
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            printf("* ");
+        }
+        for(int j=1;j<=2*(n-i);j++){
+            printf("  ");
+        }
+        for(int j=1;j<=i;j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
     return 0;
 }
 
