ab200125624495ea33fba623bd47c39bc03c6677
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-06-08 11:56:09 +0530

    Write a program to copy the contents of a file into another file.

--- imp/file.c
+++ imp/file.c
@@ -6,36 +6,31 @@ int main() {
     FILE *sourceFile, *targetFile;
     char ch;
 
-    // Input source and target file names
     printf("Enter the source file name: ");
     scanf("%s", sourceFileName);
 
     printf("Enter the target file name: ");
     scanf("%s", targetFileName);
 
-    // Open source file in read mode
     sourceFile = fopen(sourceFileName, "r");
     if (sourceFile == NULL) {
-        printf("Cannot open source file %s\n", sourceFileName);
-        exit(1);
+      printf("Cannot open source file %s\n", sourceFileName);
+      exit(1);
     }
 
-    // Open target file in write mode
     targetFile = fopen(targetFileName, "w");
     if (targetFile == NULL) {
-        printf("Cannot open target file %s\n", targetFileName);
-        fclose(sourceFile);
-        exit(1);
+      printf("Cannot open target file %s\n", targetFileName);
+      fclose(sourceFile);
+      exit(1);
     }
 
-    // Copy contents from source to target
     while ((ch = fgetc(sourceFile)) != EOF) {
-        fputc(ch, targetFile);
+      fputc(ch, targetFile);
     }
 
     printf("File copied successfully from %s to %s.\n", sourceFileName, targetFileName);
 
-    // Close both files
     fclose(sourceFile);
     fclose(targetFile);
 
