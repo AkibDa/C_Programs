e63eb552fb30895ba2cb68e24815125251b307c6
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-08-17 10:11:20 +0530

    Write a C program to convert an infix expression taken from the user to a postfix expression and print it.

--- class/infix_to_postfix.c
+++ class/infix_to_postfix.c
@@ -1,86 +1,95 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <ctype.h>
+#include <string.h>
 
-#define MAX_SIZE 100
+#define MAX_SIZE 10
 
+// Stack implementation
 char stack[MAX_SIZE];
 int top = -1;
 
-void push(char op) {
-  stack[++top] = op;
+void push(char item) {
+  if (top >= MAX_SIZE - 1) {
+    printf("Stack Overflow\n");
+    return;
+  }
+  stack[++top] = item;
 }
 
 char pop() {
-  if (top == -1) {
+  if (top < 0) {
+    printf("Stack Underflow\n");
     return '\0';
   }
   return stack[top--];
 }
 
-int isOperator(char ch) {
-  return (ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '^');
-}
-
-int precedence(char ch) {
-  if (ch == '+' || ch == '-') {
-    return 1;
-  } else if (ch == '*' || ch == '/') {
-    return 2;
-  } else if (ch == '^') {
-    return 3;
+int precedence(char operator) {
+  switch (operator) {
+    case '+':
+    case '-':
+      return 1;
+    case '*':
+    case '/':
+      return 2;
+    case '^':
+      return 3;
+    default:
+      return -1;
   }
-  return 0;
 }
 
-void infixToPostfix(char* infix) {
-  int i, j;
-  char postfix[MAX_SIZE];
-  i = j = 0;
+int isOperator(char ch) {
+  return ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '^';
+}
 
-  while (infix[i] != '\0') {
-    if (isalnum(infix[i])) {
-      postfix[j++] = infix[i];
-    } else if (infix[i] == '(') {
-      push(infix[i]);
-    } else if (infix[i] == ')') {
-        while (top != -1 && stack[top] != '(') {
-          postfix[j++] = pop();
-        }
-        if (top != -1 && stack[top] == '(') {
-          pop(); // Pop '('
-        }
-    } else if (isOperator(infix[i])) {
-        while (top != -1 && precedence(stack[top]) >= precedence(infix[i])) {
-          postfix[j++] = pop();
-        }
-        push(infix[i]);
+void infixToPostfix(char* infix, char* postfix) {
+  int i, j = 0;
+  char ch, popped;
+
+  for (i = 0; infix[i] != '\0'; i++) {
+    ch = infix[i];
+
+    if (ch == ' ') {
+      continue; // skip whitespace
     }
-    i++;
+    else if (isalnum(ch)) {
+      postfix[j++] = ch;
+    }
+    else if (ch == '(') {
+      push(ch);
+    }
+    else if (ch == ')') {
+      while ((popped = pop()) != '(') {
+        postfix[j++] = popped;
+      }
+    }
+    else if (isOperator(ch)) {
+      while (top != -1 && precedence(ch) <= precedence(stack[top])) {
+        postfix[j++] = pop();
+      }
+      push(ch);
+    }
   }
 
   while (top != -1) {
-    postfix[j++] = pop();
+      postfix[j++] = pop();
   }
-  postfix[j] = '\0';
 
-  printf("Postfix expression: %s\n", postfix);
+  postfix[j] = '\0'; 
 }
 
 int main() {
-  char *expression;
-  expression = (char *)malloc(MAX_SIZE * sizeof(char));
-  if (expression == NULL) {
-    fprintf(stderr, "Memory allocation failed\n");
-    return 1;
-  }
-  printf("Infix to Postfix Converter\n");
-  printf("Enter an infix expression: ");
-  fgets(expression, MAX_SIZE, stdin);
-  expression[strcspn(expression, "\n")] = '\0';
+  char infix[MAX_SIZE], postfix[MAX_SIZE];
 
-  infixToPostfix(expression);
+  printf("Enter an infix expression: ");
+  fgets(infix, MAX_SIZE, stdin);
+  infix[strcspn(infix, "\n")] = '\0'; 
+
+  infixToPostfix(infix, postfix);
+
+  printf("Postfix expression: %s\n", postfix);
 
   return 0;
 }
