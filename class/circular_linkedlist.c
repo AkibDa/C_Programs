511edcd95fb1b1ec2452ce00e05165989cbf7eb5
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-09-11 19:48:27 +0530

    added functions to delete and add at any position

--- class/circular_linkedlist.c
+++ class/circular_linkedlist.c
@@ -65,6 +65,29 @@ void insertAtEnd(int data){
   printf("%d is inserted in the beginning.\n",data);
 }
 
+void insertAtPosition(int data, int position){
+  struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+  newNode->data = data;
+  if(position == 1){
+    insertAtStart(data);
+    return;
+  }
+  struct Node* temp = head;
+  int count = 1;
+  while(count < position-1 && temp->next != head){
+    temp = temp->next;
+    count++;
+  }
+  if(count != position-1){
+    printf("Position is greater than the length of the linkedlist. Inserting at the end.\n");
+    insertAtEnd(data);
+    return;
+  }
+  newNode->next = temp->next;
+  temp->next = newNode;
+  printf("%d is inserted at position %d.\n",data,position);
+}
+
 void deleteAtStart(){
   if(head == NULL){
     printf("No value present to delete.\n");
@@ -109,6 +132,33 @@ void deleteAtEnd(){
   printf("Deleted the last element.\n");
 }
 
+void deleteAtPosition(int position){
+  if(head == NULL){
+    printf("No value present to delete.\n");
+    return;
+  }
+  if(position == 1){
+    deleteAtStart();
+    return;
+  }
+  struct Node* temp = head;
+  struct Node* pretemp = head;
+  int count = 1;
+  while(count < position && temp->next != head){
+    pretemp = temp;
+    temp = temp->next;
+    count++;
+  }
+  if(count != position){
+    printf("Position is greater than the length of the linkedlist. Deleting at the end.\n");
+    deleteAtEnd();
+    return;
+  }
+  pretemp->next = temp->next;
+  free(temp);
+  printf("Deleted element at position %d.\n",position);
+}
+
 void display(){
   if(head == NULL){
     printf("No value present to display.\n");
@@ -124,41 +174,53 @@ void display(){
 }
 
 int main(){
-  int choice,data;
+  int choice,data, position;
   do{
     printf("Menu For Linkedlist functions:\n");
-    printf("0: Create Linkedlist\n1: Insert at beginning\n2: Insert at end\n3: Delete at start\n4: Delete at end\n5: Display\n6: Exit\n");
+    printf("1: Create Linkedlist\n2: Insert at beginning\n3: Insert at end\n4: Insert at any position\n5: Delete at start\n6: Delete at end\n7: Delete at any position\n8: Display\n9: Exit\n");
     printf("Enter your choice: ");
     scanf("%d",&choice);
     switch(choice){
-      case 0:
+      case 1:
         createLinkedList();
         break;
-      case 1:
+      case 2:
         printf("Enter the data u want to insert: ");
         scanf("%d",&data);
         insertAtStart(data);
         break;
-      case 2:
+      case 3:
         printf("Enter the data u want to insert: ");
         scanf("%d", &data);
         insertAtEnd(data);
         break;
-      case 3:
-        deleteAtStart();
-        break;
       case 4:
-        deleteAtEnd();
+        printf("Enter the position where u want to insert: ");
+        scanf("%d", &position);
+        printf("Enter the data u want to insert: ");
+        scanf("%d", &data);
+        insertAtPosition(data, position);
         break;
       case 5:
-        display();
+        deleteAtStart();
         break;
       case 6:
+        deleteAtEnd();
+        break;
+      case 7:
+        printf("Enter the position where u want to delete: ");
+        scanf("%d", &position);
+        deleteAtPosition(position);
+        break;
+      case 8:
+        display();
+        break;
+      case 9:
         printf("Exiting the program...\n");
         break;
       default:
         printf("Invalid option !\n");
     }
-  }while(choice != 6);
+  }while(choice != 9);
   return 0;
 }
